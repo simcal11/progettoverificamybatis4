16a90222a9d3391b6ffd89a1d78a9a21a43f52fd
simcal11 <simonecalosc11@gmail.com>
2024-08-07 15:59:39 +0200

    Ordinati i metodi dei controller
    Ordinati i metodi dei controller, prima i GET, poi i POST, poi PUT, poi DELETE

--- src/main/java/it/eagleprojects/progettoverificamybatis4/controller/CorsoController.java
+++ src/main/java/it/eagleprojects/progettoverificamybatis4/controller/CorsoController.java
@@ -14,24 +14,18 @@ import it.eagleprojects.progettoverificamybatis4.dao.CorsoMapper;
 import it.eagleprojects.progettoverificamybatis4.dao.StudenteMapper;
 import it.eagleprojects.progettoverificamybatis4.model.Corso;
 
-
-
-
 @Controller
 public class CorsoController {
-	
+
 	@Autowired
 	CorsoMapper corsoMapper;
-	
+
 	@Autowired
 	StudenteMapper studenteMapper;
 
-	
-
-	
+	// Metodi GET
 	/* _____________________________________________________________________ */
-	
-	
+
 	@RequestMapping(value = "/corsi", method = RequestMethod.GET, produces = "application/json")
 	public @ResponseBody List<Corso> getAllCorsi() {
 		return corsoMapper.getAllCorsi();
@@ -39,123 +33,113 @@ public class CorsoController {
 
 	@RequestMapping(value = "/corsi/{corsoId}", method = RequestMethod.GET)
 	public @ResponseBody Corso getCorsoId(@PathVariable("corsoId") long corsoId) throws Exception {
-
 		Corso corso = corsoMapper.getCorsoById(corsoId);
 
 		if (corso == null) {
 			throw new Exception("Non esiste un Corso con id = " + corsoId);
 		}
-
 		return corso;
 	}
-	
 
-	
-	@RequestMapping(value = "/studenti/{studenteId}/corsi", method = RequestMethod.GET, produces  = "application/json")
-	public @ResponseBody List<Corso> getAllCorsiByStudenteId(@PathVariable("studenteId") long studenteId) throws Exception {
-		
-		
-		if(studenteMapper.getStudenteById(studenteId) == null) {
+	@RequestMapping(value = "/studenti/{studenteId}/corsi", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody List<Corso> getAllCorsiByStudenteId(@PathVariable("studenteId") long studenteId)
+			throws Exception {
+		if (studenteMapper.getStudenteById(studenteId) == null) {
 			throw new Exception("Non esiste uno Studente con id = " + studenteId);
-		}
-		else {
+		} else {
 			return corsoMapper.getAllCorsiByStudenteId(studenteId);
 		}
-		
 	}
-	
-	/*************************************************************************************/
-	
-	@RequestMapping(value = "/corsi/{corsoId}", method = RequestMethod.PUT, produces  = "application/json")
-	public @ResponseBody Corso updateCorsoById(@PathVariable("corsoId") long corsoId, @RequestBody Corso corsoRequest) throws Exception {
-		
+
+	// Metodi POST
+	/* _____________________________________________________________________ */
+
+	@RequestMapping(value = "/corsi", method = RequestMethod.POST, consumes = "application/json")
+	public @ResponseBody Corso saveCorso(@RequestBody Corso corso) {
+		return corsoMapper.saveCorso(corso);
+	}
+
+	@RequestMapping(value = "/studenti/{studenteId}/corsi", method = RequestMethod.POST, consumes = "application/json")
+	public @ResponseBody void addCorsoToStudente(@PathVariable(value = "studenteId") long studenteId,
+			@RequestBody Corso corso) throws Exception {
+		if (studenteMapper.getStudenteById(studenteId) == null) {
+			throw new Exception("Non esiste uno Studente con id = " + studenteId);
+		}
+
+		if (corso.getId() == null) {
+			throw new Exception("Non è stato specifica l'id del corso da aggiungere " + corso.getId());
+		}
+
+		if (corsoMapper.getCorsoById(corso.getId()) == null) {
+			throw new Exception("Non esiste un Corso con id = " + corso.getId());
+		}
+
+		corsoMapper.addCorsoToStudente(studenteId, corso);
+	}
+
+	// Metodi PUT
+	/* _____________________________________________________________________ */
+
+	@RequestMapping(value = "/corsi/{corsoId}", method = RequestMethod.PUT, produces = "application/json")
+	public @ResponseBody Corso updateCorsoById(@PathVariable("corsoId") long corsoId, @RequestBody Corso corsoRequest)
+			throws Exception {
+
 		Corso corsoDaAggiornare = corsoMapper.getCorsoById(corsoId);
 
 		if (corsoDaAggiornare == null) {
 			throw new Exception("Non esiste un Corso con id = " + corsoId);
 		}
-		
-		corsoRequest.setId(corsoDaAggiornare.getId());
-		
-		if(corsoRequest.getNome() ==null) {
-			corsoRequest.setNome(corsoDaAggiornare.getNome() );
-		}
-		
-		if(corsoRequest.getCfu() ==null) {
-			corsoRequest.setCfu(corsoDaAggiornare.getCfu() );
-		}
-		
-		if(corsoRequest.getOre() ==null) {
-			corsoRequest.setOre(corsoDaAggiornare.getOre() );
-		}
-		
 
-		
+		corsoRequest.setId(corsoDaAggiornare.getId());
+
+		if (corsoRequest.getNome() == null) {
+			corsoRequest.setNome(corsoDaAggiornare.getNome());
+		}
+
+		if (corsoRequest.getCfu() == null) {
+			corsoRequest.setCfu(corsoDaAggiornare.getCfu());
+		}
+
+		if (corsoRequest.getOre() == null) {
+			corsoRequest.setOre(corsoDaAggiornare.getOre());
+		}
 		return corsoMapper.updateCorsoById(corsoRequest);
 	}
-	
-	@RequestMapping(value = "/corsi", method = RequestMethod.POST, consumes = "application/json")
-	public @ResponseBody Corso saveCorso(@RequestBody Corso corso) {
-		
-		return corsoMapper.saveCorso(corso);
-	}
-	
-	
-	@RequestMapping(value = "/studenti/{studenteId}/corsi", method = RequestMethod.POST, consumes = "application/json")
-	public @ResponseBody void addCorsoToStudente(@PathVariable(value = "studenteId") long studenteId, @RequestBody Corso corso) throws Exception {
-		
-		if(studenteMapper.getStudenteById(studenteId) == null) {
-			throw new Exception("Non esiste uno Studente con id = " + studenteId);
-		}
-		
-		if(corso.getId() == null) {
-			throw new Exception("Non è stato specifica l'id del corso da aggiungere " + corso.getId());
-		}
-		
-		if(corsoMapper.getCorsoById(corso.getId()) == null) {
-			throw new Exception("Non esiste un Corso con id = " + corso.getId());
-		}
-		
-		corsoMapper.addCorsoToStudente(studenteId, corso);
-	}
-	
-	@RequestMapping(value = "/corsi/{corsoId}", method = RequestMethod.DELETE)
-	public @ResponseBody void deleteCorso(@PathVariable("corsoId") Long corsoId) throws Exception {
-		
-		if(corsoMapper.getCorsoById(corsoId) == null) {
-			throw new Exception("Non esiste un Corso con id = " + corsoId);
-		}
-		
-		corsoMapper.deleteCorsoById(corsoId);
-	}
-	
+
+	// Metodi DELETE
+	/* _____________________________________________________________________ */
+
 	@RequestMapping(value = "/corsi", method = RequestMethod.DELETE)
 	public @ResponseBody void deleteAllCorsi() throws Exception {
 		corsoMapper.deleteAllCorsi();
 	}
-	
+
+	@RequestMapping(value = "/corsi/{corsoId}", method = RequestMethod.DELETE)
+	public @ResponseBody void deleteCorso(@PathVariable("corsoId") Long corsoId) throws Exception {
+		if (corsoMapper.getCorsoById(corsoId) == null) {
+			throw new Exception("Non esiste un Corso con id = " + corsoId);
+		}
+
+		corsoMapper.deleteCorsoById(corsoId);
+	}
+
 	@RequestMapping(value = "/studenti/{studenteId}/corsi", method = RequestMethod.DELETE, consumes = "application/json")
-	public @ResponseBody void deleteCorsoFromStudente(@PathVariable(value = "studenteId") Long studenteId, @RequestBody Corso corso) throws Exception {
-		
-		if(studenteMapper.getStudenteById(studenteId) == null) {
+	public @ResponseBody void deleteCorsoFromStudente(@PathVariable(value = "studenteId") Long studenteId,
+			@RequestBody Corso corso) throws Exception {
+
+		if (studenteMapper.getStudenteById(studenteId) == null) {
 			throw new Exception("Non esiste uno Studente con id = " + studenteId);
 		}
-		
-		if(corso.getId() == null) {
+
+		if (corso.getId() == null) {
 			throw new Exception("Non è stato specifica l'id del Corso da eliminare " + corso.getId());
 		}
-		
-		if(corsoMapper.getCorsoById(corso.getId()) == null) {
+
+		if (corsoMapper.getCorsoById(corso.getId()) == null) {
 			throw new Exception("Non esiste un Corso con id = " + corso.getId());
 		}
-		
+
 		corsoMapper.deleteCorsoFromStudente(studenteId, corso);
 	}
-	
-	
-	
-	
-	
 
-	
 }
--- src/main/java/it/eagleprojects/progettoverificamybatis4/controller/StudenteController.java
+++ src/main/java/it/eagleprojects/progettoverificamybatis4/controller/StudenteController.java
@@ -21,10 +21,12 @@ public class StudenteController {
 
 	@Autowired
 	StudenteMapper studenteMapper;
-	
+
 	@Autowired
 	CorsoMapper corsoMapper;
 
+	// Metodi GET
+	/* _____________________________________________________________________ */
 
 	@RequestMapping(value = "/studenti", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<List<Studente>> getAllStudenti() {
@@ -33,117 +35,121 @@ public class StudenteController {
 
 	@RequestMapping(value = "/studenti/{studenteId}", method = RequestMethod.GET)
 	public @ResponseBody Studente getStudenteById(@PathVariable("studenteId") Long studenteId) throws Exception {
-
 		Studente studente = studenteMapper.getStudenteById(studenteId);
 
 		if (studente == null) {
 			throw new Exception("Non esiste uno Studente con id = " + studenteId);
 		}
-
 		return studente;
-
 	}
-	
-	@RequestMapping(value = "/corsi/{corsoId}/studenti", method = RequestMethod.GET, produces  = "application/json")
-	public @ResponseBody  List<Studente> getAllStudentiByCorsoId(@PathVariable("corsoId") Long corsoId) throws Exception {
-		
-		
-		if(corsoMapper.getCorsoById(corsoId) == null) {
+
+	@RequestMapping(value = "/corsi/{corsoId}/studenti", method = RequestMethod.GET, produces = "application/json")
+	public @ResponseBody List<Studente> getAllStudentiByCorsoId(@PathVariable("corsoId") Long corsoId)
+			throws Exception {
+		if (corsoMapper.getCorsoById(corsoId) == null) {
 			throw new Exception("Non esiste un Corso con id = " + corsoId);
-		}
-		else {
+		} else {
 			return studenteMapper.getAllStudentiByCorsoId(corsoId);
 		}
-		
 	}
-	
-	@RequestMapping(value = "/studenti/{studenteId}", method = RequestMethod.PUT, produces  = "application/json")
-	public @ResponseBody Studente updateStudenteById(@PathVariable("studenteId") Long studenteId, @RequestBody Studente studenteRequest) throws Exception {
-		
+
+	// Metodi POST
+	/* _____________________________________________________________________ */
+
+	@RequestMapping(value = "/studenti", method = RequestMethod.POST, consumes = "application/json")
+	public @ResponseBody Studente saveStudente(@RequestBody Studente studente) {
+
+		return studenteMapper.saveStudente(studente);
+	}
+
+	@RequestMapping(value = "/corsi/{corsoId}/studenti", method = RequestMethod.POST, consumes = "application/json")
+	public @ResponseBody void addStudenteToCorso(@PathVariable(value = "corsoId") Long corsoId,
+			@RequestBody Studente studente) throws Exception {
+
+		if (corsoMapper.getCorsoById(corsoId) == null) {
+			throw new Exception("Non esiste un Corso con id = " + corsoId);
+		}
+
+		if (studente.getId() == null) {
+			throw new Exception("Non è stato specifica l'id dello studente da aggiungere " + studente.getId());
+		}
+
+		if (studenteMapper.getStudenteById(studente.getId()) == null) {
+			throw new Exception("Non esiste uno Studente con id = " + studente.getId());
+		}
+
+		studenteMapper.addStudenteToCorso(corsoId, studente);
+	}
+
+	// Metodi PUT
+	/* _____________________________________________________________________ */
+
+	@RequestMapping(value = "/studenti/{studenteId}", method = RequestMethod.PUT, produces = "application/json")
+	public @ResponseBody Studente updateStudenteById(@PathVariable("studenteId") Long studenteId,
+			@RequestBody Studente studenteRequest) throws Exception {
+
 		Studente studenteDaAggiornare = studenteMapper.getStudenteById(studenteId);
 
 		if (studenteDaAggiornare == null) {
 			throw new Exception("Non esiste uno Studente con id = " + studenteId);
 		}
-		
+
 		studenteRequest.setId(studenteDaAggiornare.getId());
-		
-		if(studenteRequest.getNome() ==null) {
-			studenteRequest.setNome(studenteDaAggiornare.getNome() );
+
+		if (studenteRequest.getNome() == null) {
+			studenteRequest.setNome(studenteDaAggiornare.getNome());
 		}
-		
-		if(studenteRequest.getCognome() ==null) {
-			studenteRequest.setCognome(studenteDaAggiornare.getCognome() );
+
+		if (studenteRequest.getCognome() == null) {
+			studenteRequest.setCognome(studenteDaAggiornare.getCognome());
 		}
-		
-		if(studenteRequest.getEmail() ==null) {
-			studenteRequest.setEmail(studenteDaAggiornare.getEmail() );
+
+		if (studenteRequest.getEmail() == null) {
+			studenteRequest.setEmail(studenteDaAggiornare.getEmail());
 		}
-		
-		if(studenteRequest.getMatricola() ==null) {
-			studenteRequest.setMatricola(studenteDaAggiornare.getMatricola() );
+
+		if (studenteRequest.getMatricola() == null) {
+			studenteRequest.setMatricola(studenteDaAggiornare.getMatricola());
 		}
-		
+
 		return studenteMapper.updateStudenteById(studenteRequest);
 	}
-	
-	@RequestMapping(value = "/studenti", method = RequestMethod.POST, consumes = "application/json")
-	public @ResponseBody Studente saveStudente(@RequestBody Studente studente) {
-		
-		return studenteMapper.saveStudente(studente);
-	}
-	
-	
-	@RequestMapping(value = "/corsi/{corsoId}/studenti", method = RequestMethod.POST, consumes = "application/json")
-	public @ResponseBody void addStudenteToCorso(@PathVariable(value = "corsoId") Long corsoId, @RequestBody Studente studente) throws Exception {
-		
-		if(corsoMapper.getCorsoById(corsoId) == null) {
-			throw new Exception("Non esiste un Corso con id = " + corsoId);
-		}
-		
-		if(studente.getId() == null) {
-			throw new Exception("Non è stato specifica l'id dello studente da aggiungere " + studente.getId());
-		}
-		
-		if(studenteMapper.getStudenteById(studente.getId()) == null) {
-			throw new Exception("Non esiste uno Studente con id = " + studente.getId());
-		}
-		
-		studenteMapper.addStudenteToCorso(corsoId, studente);
-	}
-	
+
+	// Metodi DELETE
+	/* _____________________________________________________________________ */
+
 	@RequestMapping(value = "/studenti/{studenteId}", method = RequestMethod.DELETE)
 	public @ResponseBody void deleteStudente(@PathVariable("studenteId") Long studenteId) throws Exception {
-		
-		if(studenteMapper.getStudenteById(studenteId) == null) {
+
+		if (studenteMapper.getStudenteById(studenteId) == null) {
 			throw new Exception("Non esiste uno Studente con id = " + studenteId);
 		}
-		
+
 		studenteMapper.deleteStudenteById(studenteId);
 	}
-	
+
 	@RequestMapping(value = "/studenti", method = RequestMethod.DELETE)
 	public @ResponseBody void deleteAllStudenti() throws Exception {
 		studenteMapper.deleteAllStudenti();
 	}
-	
+
 	@RequestMapping(value = "/corsi/{corsoId}/studenti", method = RequestMethod.DELETE, consumes = "application/json")
-	public @ResponseBody void deleteStudenteFromCorso(@PathVariable(value = "corsoId") Long corsoId, @RequestBody Studente studente) throws Exception {
-		
-		if(corsoMapper.getCorsoById(corsoId) == null) {
+	public @ResponseBody void deleteStudenteFromCorso(@PathVariable(value = "corsoId") Long corsoId,
+			@RequestBody Studente studente) throws Exception {
+
+		if (corsoMapper.getCorsoById(corsoId) == null) {
 			throw new Exception("Non esiste un Corso con id = " + corsoId);
 		}
-		
-		if(studente.getId() == null) {
+
+		if (studente.getId() == null) {
 			throw new Exception("Non è stato specifica l'id dello studente da eliminare " + studente.getId());
 		}
-		
-		if(studenteMapper.getStudenteById(studente.getId()) == null) {
+
+		if (studenteMapper.getStudenteById(studente.getId()) == null) {
 			throw new Exception("Non esiste uno Studente con id = " + studente.getId());
 		}
-		
+
 		studenteMapper.deleteStudenteFromCorso(corsoId, studente);
 	}
-	
 
 }
